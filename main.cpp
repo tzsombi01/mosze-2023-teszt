2469025ef5717bb3671157aec97f277b6cca5a9d
tzsombi01 <toreky.zsombor@gmail.com>
2023-09-17 20:23:37 +0200

    feature(code-correction): Corrected mistakes

--- main.cpp
+++ main.cpp
@@ -4,38 +4,26 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    // Typo: The NELEMENTS should be N_ELEMENTS
-    int *b = new int[NELEMENTS];
+    int *b = new int[N_ELEMENTS];
 
-    // End of line missing character: ';'
-    // Bad character(s): ''' instead of '"'
-    std::cout << '1-100 ertekek duplazasa'
+    std::cout << "1-100 ertekek duplazasa";
 
-    // Loop is missing 2 arguments
-    for (int i = 0;)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
 
-    // Endless loop, exit parameter is faulty
-    for (int i = 0; i; i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-
-        // End of line missing character: ';'
-        // The value is not getting logged onto console
-        std::cout << "Ertek:"
+        std::cout << "Ertek: " << b[i];
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
 
-    // Variable "atlag" is not getting initialised
-    int atlag;
+    int atlag = 0;
 
-    // Bad character(s): ',' instead of ';'
-    for (int i = 0; i < N_ELEMENTS, i++)
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-
-        // End of line missing character: ';'
-        atlag += b[i]
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
